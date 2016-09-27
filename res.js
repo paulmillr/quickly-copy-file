1f015bc79a6b8fc129454c16fa5922ec853b7219
Paul Miller <paul@paulmillr.com>
2016-09-27 16:25:08 +0300

    Delete res.js

--- res.js
+++
@@ -1,9 +0,0 @@
-import {statSync, readFileSync} from 'fs';
-import test from 'ava';
-import copyFile from './';
-
-test('Basic copying', async t => {
-  await copyFile('test.js', 'res.js')
-  t.ok(statSync('res.js'));
-  t.same(readFileSync('test.js'), readFileSync('res.js'));
-});
